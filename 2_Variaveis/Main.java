8f5fee75172781fa06ee6f43f6e47d9177d447f1
alexsanderbueno <abcbueno007@gmail.com>
2026-08-19 22:16:53 -0300

    revisão de fundamentos

---
+++ 2_Variaveis/Main.java
@@ -0,0 +1,26 @@
+public class Main{
+    public static void main (String[] args){
+        String nome = "Savalo";
+        String sobrenome = "Horse";
+        int idade = 85;
+        double altura = 1.80;
+        boolean empregado=true;
+        String telefone="11 3004-4441";
+        String endereco= "alameda dos cavalos";
+        String cep="18185000";
+        String cidade="Tatui";
+        String uf="SP";
+        double salario_bruto=51000;
+        double valor_hora_extra=510;
+        System.out.println("Nome do aluno: " + nome + " " + sobrenome);
+        System.out.println("Idade:" + idade);
+        System.out.println("Empregado:" + empregado);
+        System.out.println("Telefone:" + telefone);
+        System.out.println("Endereco:" + endereco);
+        System.out.println("CEP:" + cep);
+        System.out.println("Cidade:" + cidade);
+        System.out.println("UF:" + uf);
+        System.out.println("Salário Bruto:"+ salario_bruto);
+        System.out.println("Valor hora extra:" + valor_hora_extra);
+    }
+}
